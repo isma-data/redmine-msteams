1e032a6aab5cc95086526556e4e2c470430f9063
isma-data <dominik.pegler@inject-star.com>
2020-12-17 12:59:52 +0100

    edit

--- init.rb
+++ init.rb
@@ -3,13 +3,12 @@ require 'redmine_msteams/listener'
 
 Redmine::Plugin.register :redmine_msteams do
   name 'Redmine Microsoft Teams'
-  author 'Sebastian Wahl'
-  url 'https://github.com/simplethings/redmine-msteams'
-  author_url 'http://www.simplethings.de'
-  description 'Microsoft Teams chat integration'
-  version '0.2.0'
+  author 'isma-data'
+  url 'https://github.com/isma-data/redmine-msteams'
+  description 'Microsoft Teams chat integration based on the plugin by Sebastian Wahl'
+  version '0.5.0'
 
-  requires_redmine :version_or_higher => '3.3.0'
+  requires_redmine :version_or_higher => '4.1.0'
 
   settings :default => { 'display_watchers' => 'no' },
            :partial => 'settings/msteams_settings'
